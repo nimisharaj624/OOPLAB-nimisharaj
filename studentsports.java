6855df616e4f4d2f07831a82293cf7f580b847ef
nimisharaj624 <76961905+nimisharaj624@users.noreply.github.com>
2021-06-10 13:28:54 +0530

    Add files via upload

---
+++ studentsports.java
@@ -0,0 +1,59 @@
+import java.util.*;
+interface Student
+ {
+    Scanner sc = new Scanner(System.in);
+    int roll = 1;
+    String name="Nimisha",course="MCA";
+
+}
+class Sports
+{
+    Scanner sc = new Scanner(System.in);
+    int mark;
+    void read()
+    {
+        System.out.println("Enter mark:");
+        mark=sc.nextInt();
+    }
+}
+
+class Result extends Sports implements Student
+{
+    void displ()
+    {
+        System.out.println("Details of student:");
+		 System.out.println("Roll number:"+roll);
+        System.out.println("Name:"+name);
+        System.out.println("Class Name:"+course);
+        System.out.println("Academic Mark:"+mark);
+    }
+}
+class Studentdet
+ {
+    public static void main(String args[])
+    {
+        Result res = new Result();
+        res.read();
+        res.displ();
+    }
+}
+
+/*
+
+---------------------------------------------------------
+
+
+
+Output
+
+Enter mark: 50
+
+Details of student:
+
+Roll number:1
+Name: Nimisha
+Class Name: MCA
+Academic Mark: 50
+
+
+*/
